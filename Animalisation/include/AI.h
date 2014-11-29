04d47ed39323c93a7e5d0a3185963d4972d86951
Harry Friend <harryfriend@hotmail.co.uk>
2014-11-29 04:33:13 +0000

    AI movement finished. Needs testing Compiles fine.

--- Animalisation/include/AI.h
+++ Animalisation/include/AI.h
@@ -2,67 +2,83 @@
 #define AI_H
 
 #include <vector>
-//
-//class AI
-//{
-//private:
-//
-//	bool aiState; //AI off or on
-//
-//	const float unfavTerrain = 0.7;
-//	const float favTerrain = 1.5;
-//
-//	std::vector<int> IDs;
-//
-//public:
-//	AI();
-//	~AI();
-//	/**
-//	@brief A method to check the nearby Hexagon grid tiles, and find out which tiles are adjacent
-//
-//	The method takes in the HexagonGrid, and checks the IDs of the grids to find the ones next to the origin grid.
-//	The Origin grid is the one that the object that called the AI is currently on.
-//	@param HexagonGrid - Temp
-//	*/
-//	void getNearbyHex(/*&HexagonGrid*/); 
-//	/**
-//	@brief A method to check the nearby adjacent hexagon tiles for enemies
-//
-//	This method uses the information from getNearbyHex to check for enemies.
-//	(Possible that grid stores a state if it is currently occupied with a object.)
-//	@param HexagonGrid - Temp
-//	*/
-//	void getNearbyEnemies(/*&HexagonGrid*/); 
-//	/**
-//	@brief A method to check the tactical value of the adjacent hexagon tiles
-//
-//	This method uses the information from getNearbyHex to check what type of tile it is, and assign any modifies to it.
-//	@param HexagonGrid - Temp
-//	*/
-//	void checkTacticalValue(/*&HexagonGrid*/);
-//	void setMovement();
-//	void clearVectors();
-//	/**
-//	@brief A method to initialize, and carry out the Movement AI
-//
-//	This method calls different methods to get information about nearby tiles, and decide which si the best to move to.
-//	This sets the state for AI to false if all objects that can move have finished.
-//	*/
-//	void MovementAI(); 
-//	/**
-//	@brief A method to check the list of hubs that the race owns for those that are inactive
-//
-//	This method has a parameter that contains all the hubs the race owns, and checks if their inactive state has been set to true.
-//	@param HubList - Temp
-//	*/
-//	void getAvailableHub(/*&HubList*/);
-//	/**
-//	@brief
-//	*/
-//	void addBuildQueue(); //Checks for what object at the hub will build the fastest, and adds it to its build queue setting the queue state to false
-//	void HubAI(); //Main Hub AI method (will most likely have this call the other methods, and finalize the build queues for the different hubs)
+#include "Terrain.h"
 
-}
+class AI
+{
+private:
+
+	bool aiState; //AI off or on
+
+	const float impasTerrain = 0.4;
+	const float unfavTerrain = 0.7;
+	const float favTerrain = 1.5;
+
+	int mapHeight;
+	int mapWidth;
+	std::vector<tile> tileGrid;
+	std::vector<int>ExeceptionListLeft;
+	std::vector<int>ExeceptionListRight;
+	std::vector<int>ExeceptionListDown;
+	std::vector<int>ExeceptionListTop;
+public:
+	AI(int Height, int Width);
+	~AI();
+	/**
+	@brief A method to check the nearby grid tiles, and find out which tiles are adjacent
+
+	The method takes in the Grid, and checks the IDs of the grids to find the ones next to the origin grid.
+	The Origin grid is the one that the object that called the AI is currently on.
+	@param Grid - Temp
+	*/
+	void getNearbySquare(std::vector<tile> & tiles, int index);
+	/**
+	@brief A method to check the nearby adjacent tiles for enemies
+
+	This method uses the information from getNearbySquare to check for enemies.
+	(Possible that grid stores a state if it is currently occupied with a object.)
+	@param Grid - Temp
+	*/
+	bool getOccupied(std::vector<tile>&tileGrid,int i);
+	/**
+	@brief A method to check the tactical value of the adjacent hexagon tiles
+
+	This method uses the information from getNearbySquare to check what type of tile it is, and assign any modifies to it.
+	@param Grid - Temp
+	*/
+	int checkTacticalValue(std::vector<tile>&tilegrid, int i);
+	void getExceptionSquare();
+	tile setMovement(tile move);
+	void clearVectors();
+	/**
+	@brief A method to initialize, and carry out the Movement AI
+
+	This method calls different methods to get information about nearby tiles, and decide which is the best to move to.
+	This sets the state for AI to false if all objects that can move have finished.
+	*/
+	void MovementAI(std::vector<tile>& tiles, int index);
+	/**
+	@brief A method to check the list of hubs that the race owns for those that are inactive
+
+	This method has a parameter that contains all the hubs the race owns, and checks if their inactive state has been set to true.
+	@param HubList - Temp
+	*/
+	void getAvailableHub(/*&HubList*/);
+	/**
+	@brief A method to add to the build queue of the hub that is inactive
+
+	This method checks to see what object takes the less amount of time to build.Sets the inactive flag to false on execution
+	*/
+	void addBuildQueue();
+	/**
+	@brief A method to initialize, and carry out the Hub AI
+
+	This method calls different methods to get information about hubs, and their current active flag. It then adds to their build queue,
+	and cycle through all inactive hubs until all hubs are active.
+	*/
+	void HubAI(); //Main Hub AI method (will most likely have this call the other methods, and finalize the build queues for the different hubs)
+
+};
 
 
 #endif
--- Animalisation/src/AI.cpp
+++ Animalisation/src/AI.cpp
@@ -1,80 +1,172 @@
-//#include "AI.h"
-//
-//AI::AI()
-//{
-//}
-//
-//
-//AI::~AI()
-//{
-//}
-//
-//void AI::getNearbyHex(/*&HexagonGrid*/)
-//{
-//
-//
-//}
-//
-//
-//void AI::getNearbyEnemies(/*&HexagonGrid*/)
-//{
-//	/*
-//	for (int i = 0; i < sizeof(/*vector containing enemies); i++)
-//	{
-//		if(/*Vector containing enemies Enemy state == true
-//		{
-//			IDs.push_back(/*Vector state == true ID)
-//		}
-//	}
-//	*/
-//}
-//
-//void AI::checkTacticalValue(/*&HexagonGrid*/)
-//{
-//}
-//
-//void AI::setMovement()
-//{
-//
-//}
-//
-//void AI::clearVectors()
-//{
-//	IDs.clear();
-//}
-//
-//void AI::MovementAI()
-//{
-//	/*
-//	if (aiState == true)
-//	{
-//		getNearbyHex();
-//		getNearbyEnemies();
-//
-//		for (int i = 0; i < sizeof(/*nearby hex); i++)
-//		{
-//			getNearbyHex(/*Adjectent hex tile);
-//			getNearbyEnemies(/*Adjectent hex tile);
-//			if (/*Hex tile enemies == false && tactical value >= 1)
-//			{
-//				setMovement(/*Hex tile);
-//				clearVectors();
-//				aiState = false;
-//			}
-//		}
-//	}*/
-//}
-//
-//
-//void AI::getAvailableHub()
-//{
-//}
-//
-//void AI::addBuildQueue()
-//{
-//}
-//
-//void AI::HubAI()
-//{
-//}
-//
+#include "AI.h"
+
+AI::AI(int Height, int Width)
+{
+	mapHeight = Height;
+	mapWidth = Width;
+	getExceptionSquare();
+}
+
+AI::~AI()
+{
+}
+void AI::getExceptionSquare()
+{
+	for (int i = 0; i <= mapWidth; ++i)
+	{
+		int index = 0 * mapHeight + i;
+
+		ExeceptionListTop.push_back(index);
+
+		index = i * mapHeight;
+
+		ExeceptionListDown.push_back(index);
+
+		index = i * mapHeight + mapWidth;
+
+		ExeceptionListLeft.push_back(index);
+
+		index = mapHeight * mapWidth + i;
+
+		ExeceptionListRight.push_back(index);
+	}
+
+};
+
+void AI::getNearbySquare(std::vector<tile> & tiles, int index)
+{
+
+	bool left = false, right = false, top = false, down = false;
+
+	for (int i = 0; i < mapHeight; ++i)
+	{
+		if (!index == ExeceptionListLeft[i])
+		{
+			if (left == false)
+			{
+				tileGrid.push_back(tiles[(index - 1)]);
+				left = true;
+			}
+		}
+		if (!index == ExeceptionListRight[i])
+		{
+			if (right == false)
+			{
+				tileGrid.push_back(tiles[index + 1]);
+				right = true;
+			}
+		}
+		if (!index == ExeceptionListTop[i])
+		{
+			if (top == false)
+			{
+				tileGrid.push_back(tiles[index + mapHeight]);
+				top = true;
+			}
+		}
+		if (!index == ExeceptionListDown[i])
+		{
+			if (down == false)
+			{
+				tileGrid.push_back(tiles[index - mapHeight]);
+				down = true;
+			}
+
+		}
+	}
+};
+
+
+bool AI::getOccupied(std::vector<tile>& tileGrid, int i)
+{
+	/*	if (tileGrid[i].Occupied == true)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}*/
+	return false;
+
+};
+
+int AI::checkTacticalValue(std::vector<tile>& tileGrid, int i)
+	{
+		int tile[4] = { 1, 1, 1, 1 };
+		int max = tile[0];
+
+		for (int i = 0; i < sizeof(tileGrid); i++)
+		{
+			int type = tileGrid[i].getType();
+			if (type == 0 || type == 2)
+			{
+				tile[i] = tile[i] * favTerrain;
+			}
+			else
+			{
+				if (type == 3)
+				{
+					tile[i] = tile[i] * impasTerrain;
+				}
+				else
+				{
+					tile[i] = tile[i] * unfavTerrain;
+				}
+			}
+			if (tile[i] > max)
+			{
+				max = tile[i];
+			}
+
+		}
+
+		return max;
+};
+
+	tile AI::setMovement(tile move)
+	{
+		/* may change to calling the objects setLocation method, and returning this value to it */
+		return move;
+	}
+
+	void AI::clearVectors()
+	{
+		tileGrid.clear();
+	}
+
+	void AI::MovementAI(std::vector<tile>& tiles, int index)
+	{
+		int AIindex = index;
+		aiState = true;
+		if (aiState == true)
+		{
+			getNearbySquare(tiles, AIindex);
+
+			for (int i = 0; i < sizeof(tileGrid); i++)
+			{
+				bool isOccupied = getOccupied(tileGrid, i);
+				if (isOccupied == false && checkTacticalValue(tileGrid, i) > 0.4)
+				{
+					tile move = tileGrid[i];
+					setMovement(move);
+					clearVectors();
+					aiState = false;
+				}
+			}
+		}
+	}
+
+
+	void AI::getAvailableHub()
+	{
+	}
+
+	void AI::addBuildQueue()
+	{
+	}
+
+	void AI::HubAI()
+	{
+	}
+
