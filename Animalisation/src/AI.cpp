bcc372eed1e244d35cdf6774930bd7d574eb51be
Richard Hancock <me@richardhancock.co.uk>
2014-11-29 00:38:20 +0000

    Fixed files being in the wrong directories. Started to strictly supervise pull requests and the vcxproj files. Amended git ignore to filter out more rubbish.

---
+++ Animalisation/src/AI.cpp
@@ -0,0 +1,80 @@
+//#include "AI.h"
+//
+//AI::AI()
+//{
+//}
+//
+//
+//AI::~AI()
+//{
+//}
+//
+//void AI::getNearbyHex(/*&HexagonGrid*/)
+//{
+//
+//
+//}
+//
+//
+//void AI::getNearbyEnemies(/*&HexagonGrid*/)
+//{
+//	/*
+//	for (int i = 0; i < sizeof(/*vector containing enemies); i++)
+//	{
+//		if(/*Vector containing enemies Enemy state == true
+//		{
+//			IDs.push_back(/*Vector state == true ID)
+//		}
+//	}
+//	*/
+//}
+//
+//void AI::checkTacticalValue(/*&HexagonGrid*/)
+//{
+//}
+//
+//void AI::setMovement()
+//{
+//
+//}
+//
+//void AI::clearVectors()
+//{
+//	IDs.clear();
+//}
+//
+//void AI::MovementAI()
+//{
+//	/*
+//	if (aiState == true)
+//	{
+//		getNearbyHex();
+//		getNearbyEnemies();
+//
+//		for (int i = 0; i < sizeof(/*nearby hex); i++)
+//		{
+//			getNearbyHex(/*Adjectent hex tile);
+//			getNearbyEnemies(/*Adjectent hex tile);
+//			if (/*Hex tile enemies == false && tactical value >= 1)
+//			{
+//				setMovement(/*Hex tile);
+//				clearVectors();
+//				aiState = false;
+//			}
+//		}
+//	}*/
+//}
+//
+//
+//void AI::getAvailableHub()
+//{
+//}
+//
+//void AI::addBuildQueue()
+//{
+//}
+//
+//void AI::HubAI()
+//{
+//}
+//
